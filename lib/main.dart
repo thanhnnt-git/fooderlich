4b6d946aa35dcc887748a7e8616597e41880fa61
thanh <thanhnnt0612@gmail.com>
2021-10-30 14:42:34 +0700

    setting a theme

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'fooderlich_theme.dart';
+
+void main() {
+  // 1
+  runApp(const Fooderlich());
+}
+
+class Fooderlich extends StatelessWidget {
+  // 2
+  const Fooderlich({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    final theme = FooderlichTheme.dark();
+    // TODO: Apply Home widget
+    // 3
+    return MaterialApp(
+      theme: theme,
+      title: 'Fooderlich',
+      // 4
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Fooderlich',
+            style: theme.textTheme.headline6,
+          ),
+        ),
+        body: Center(
+          child: Text('Let\'s get cooking 👩‍🍳',
+              style: theme.textTheme.headline1),
+        ),
+      ),
+    );
+  }
+}
